da72265e575c963b32d73fc1e720a6ee71adc73d
Apai <2386297795@qq.com>
2023-06-03 10:59:04 +0800

    0603

---
+++ assets/My2306.html-e0b9c707.js
@@ -0,0 +1,51 @@
+import{_ as l,p as d,q as a,s as e,R as n,t as s,Y as r,n as v}from"./framework-e1bed10d.js";const t={},o=e("h2",{id:"六月-序章",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#六月-序章","aria-hidden":"true"},"#"),n(" 六月 - 序章")],-1),c=e("p",null,"[^❤ 即使无法掌握未来，也请不要忘了明天 💖]:",-1),u=e("h2",{id:"_06-03-海屿你",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#_06-03-海屿你","aria-hidden":"true"},"#"),n(" 06-03 海屿你")],-1),m=e("p",null,[n("💟 "),e("strong",null,"好像没有了曾经的热情 这次我站来原地 即使等不到 那也好好对自己 好好的生活")],-1),b=e("blockquote",null,[e("p",null,"😄 推荐:")],-1),h={href:"https://music.163.com/song?id=1973665667&userid=588173130",target:"_blank",rel:"noopener noreferrer"},_={href:"https://music.163.com/song?id=1995456224&userid=588173130",target:"_blank",rel:"noopener noreferrer"},p=r(`<div class="language-txt line-numbers-mode" data-ext="txt"><pre class="language-txt"><code>Yeah, the universe is one big place 
+宇宙是个广袤之地
+
+It&#39;s hard to cut through all the noise
+太难突破层层噪音
+
+People tell you just to play your safe
+人们告诉你只需保险起见
+
+Like you don&#39;t even have a choice
+仿佛你根本没有选择
+
+I don&#39;t know how to belong down here
+在此我不知该如何觅得归属
+
+I don&#39;t need to be somebody to some
+我不需要做谁重要的人
+
+One day I know that I&#39;ll disappear
+我知道，终有一天我会消失
+
+So while I&#39;m here
+所以趁我还在这里
+
+I run the red lights just to see how it feels
+我闯过一盏盏红灯，只为瞧瞧那是何感受
+
+Waving my hands high, not even touching the wheel
+高高挥起我的双手，甚至没有触碰方向盘
+
+Drinking the moonlight, in the heaven I feel
+月光之下畅饮美酒，我感受天堂般的美妙
+
+Goes on forever and ever and ever
+绵延不绝，亘古永恒
+ 
+Know that I am just a drop in the ocean
+我深知我只是沧海一粟
+
+But I will ever be the waves flowing in and out
+但我宁做流动不息的浪
+
+To somewhere there on the first upon you
+去往一个前人未至之地
+
+Where people only dream about
+那里人们只敢想象罢了
+
+Drinking the moonlight, in the heaven I feel
+月光之下畅饮美酒，我感受美妙
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><img src="https://cdn.jsdelivr.net/gh/LuJunandapai/ApaiImage@main/MaYun-PicGo/image-20230603103538124.png" alt="image-20230603103538124"></p>`,2);function g(f,I){const i=v("ExternalLinkIcon");return d(),a("div",null,[o,c,u,m,b,e("ul",null,[e("li",null,[e("a",h,[n("海屿你"),s(i)]),n(" ---- 马也_Crabbit")]),e("li",null,[e("a",_,[n("时间海"),s(i)]),n(" ---- 刘大拿")])]),p])}const w=l(t,[["render",g],["__file","My2306.html.vue"]]);export{w as default};
