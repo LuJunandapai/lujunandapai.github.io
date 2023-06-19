84098d33539d0741a51179f45769c597ad1f632f
Apai <2386297795@qq.com>
2023-06-19 14:42:04 +0800

    mysql

---
+++ assets/My2306.html-893e00c1.js
@@ -0,0 +1,58 @@
+import{_ as l,p as d,q as t,s as n,R as e,t as s,Y as a,n as r}from"./framework-e1bed10d.js";const o={},c=n("h2",{id:"六月-序章",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#六月-序章","aria-hidden":"true"},"#"),e(" 六月 - 序章")],-1),u=n("p",null,"[^❤ 即使无法掌握未来，也请不要忘了明天 💖]:",-1),v=n("h2",{id:"_06-03-海屿你",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#_06-03-海屿你","aria-hidden":"true"},"#"),e(" 06-03 海屿你")],-1),m=n("p",null,[e("💟 "),n("strong",null,"好像没有了曾经的热情 这次我站来原地 即使等不到 那也好好对自己 好好的生活")],-1),p=n("blockquote",null,[n("p",null,"😄 推荐:")],-1),h={href:"https://music.163.com/song?id=1973665667&userid=588173130",target:"_blank",rel:"noopener noreferrer"},b={href:"https://music.163.com/song?id=1995456224&userid=588173130",target:"_blank",rel:"noopener noreferrer"},_=a(`<div class="language-txt line-numbers-mode" data-ext="txt"><pre class="language-txt"><code>Yeah, the universe is one big place 
+宇宙是个广袤之地
+
+It&#39;s hard to cut through all the noise
+太难突破层层噪音
+
+People tell you just to play your safe
+人们告诉你只需保险起见
+
+Like you don&#39;t even have a choice
+仿佛你根本没有选择
+
+I don&#39;t know how to belong down here
+在此我不知该如何觅得归属
+
+I don&#39;t need to be somebody to some
+我不需要做谁重要的人
+
+One day I know that I&#39;ll disappear
+我知道，终有一天我会消失
+
+So while I&#39;m here
+所以趁我还在这里
+
+I run the red lights just to see how it feels
+我闯过一盏盏红灯，只为瞧瞧那是何感受
+
+Waving my hands high, not even touching the wheel
+高高挥起我的双手，甚至没有触碰方向盘
+
+Drinking the moonlight, in the heaven I feel
+月光之下畅饮美酒，我感受天堂般的美妙
+
+Goes on forever and ever and ever
+绵延不绝，亘古永恒
+ 
+Know that I am just a drop in the ocean
+我深知我只是沧海一粟
+
+But I will ever be the waves flowing in and out
+但我宁做流动不息的浪
+
+To somewhere there on the first upon you
+去往一个前人未至之地
+
+Where people only dream about
+那里人们只敢想象罢了
+
+Drinking the moonlight, in the heaven I feel
+月光之下畅饮美酒，我感受美妙
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><img src="https://cdn.jsdelivr.net/gh/LuJunandapai/ApaiImage@main/MaYun-PicGo/image-20230603103538124.png" alt="image-20230603103538124"></p><h2 id="_06-18-我在等" tabindex="-1"><a class="header-anchor" href="#_06-18-我在等" aria-hidden="true">#</a> 06-18 我在等</h2><p>💟 等 我在等 在我可控的生活力顺其自然 等到那个人 等到花开的那天</p><blockquote><p>😄 推荐:</p></blockquote>`,5),g={href:"https://music.163.com/song?id=2008993912&userid=588173130",target:"_blank",rel:"noopener noreferrer"},k={href:"https://music.163.com/song?id=1992712131&userid=588173130",target:"_blank",rel:"noopener noreferrer"},f=a(`<div class="language-java line-numbers-mode" data-ext="java"><pre class="language-java"><code>所有给你带来痛苦的事情<span class="token punctuation">,</span>
+都是源于他人给我们灌输所谓的三观<span class="token punctuation">,</span>
+买房<span class="token punctuation">,</span> 买车<span class="token punctuation">,</span> 娶妻<span class="token punctuation">,</span> 生子<span class="token punctuation">,</span> 内卷<span class="token punctuation">,</span>
+忙忙碌碌一辈子<span class="token punctuation">,</span> 最后我们真正需要的究竟是什么<span class="token punctuation">,</span>
+也许放下固有的想法<span class="token punctuation">,</span> 理性的思考<span class="token punctuation">,</span>
+这辈子需要我们以什么样的姿态对面这个世界<span class="token punctuation">,</span> 
+什么样的人<span class="token punctuation">,</span> 什么样的事<span class="token punctuation">,</span> 配得上一路的颠沛流离<span class="token punctuation">,</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><img src="https://cdn.jsdelivr.net/gh/LuJunandapai/ApaiImage@main/My-PicGo/image-20230618163844397.png" alt="image-20230618163844397"></p>`,2);function I(w,x){const i=r("ExternalLinkIcon");return d(),t("div",null,[c,u,v,m,p,n("ul",null,[n("li",null,[n("a",h,[e("海屿你"),s(i)]),e(" ---- 马也_Crabbit")]),n("li",null,[n("a",b,[e("时间海"),s(i)]),e(" ---- 刘大拿")])]),_,n("ul",null,[n("li",null,[n("a",g,[e("i dont wanna wake up"),s(i)]),e(" ---- dollyy99")]),n("li",null,[n("a",k,[e("Time Stop"),s(i)]),e(" ---- BlackDD")])]),f])}const j=l(o,[["render",I],["__file","My2306.html.vue"]]);export{j as default};
